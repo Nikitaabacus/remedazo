7a1eb64a2fcdebfdb430ab927891c63defa2fcf7
Nikitaabacus <57934234+Nikitaabacus@users.noreply.github.com>
2019-11-21 13:05:20 +0530

    Update mainfooter.php

--- mainfooter.php
+++ mainfooter.php
@@ -157,10 +157,7 @@ $(document).ready(function()
 <script type="text/javascript" src="<?php echo BASE_URL_ROOT; ?>js/modernizr.custom.79639.js"></script>	
 
 <!-- Mobile menu script files here -->
-<script type="text/javascript" src="<?php echo BASE_URL_ROOT; ?>js/modernizr.js"></script>
-<script type="text/javascript" src="<?php echo BASE_URL_ROOT; ?>js/jquery.mobile.custom.min.js"></script>
-<script type="text/javascript" src="<?php echo BASE_URL_ROOT; ?>js/main.js"></script> 
-<script type="text/javascript" src="<?php echo BASE_URL_ROOT; ?>js/jquery-2.1.1.js"></script> 
+ 
 
 <!-- js for news -->
 <script src="<?php echo BASE_URL_ROOT; ?>js/jquery.easing.min.js"></script>
